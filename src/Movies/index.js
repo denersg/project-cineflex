8294ef1b9adbc009630422282f43156951e1568d
Dener Silva <dener.ufes@gmail.com>
2022-03-23 13:18:47 -0300

    Terminado o design das molduras dos filmes

--- src/Movies/index.js
+++ src/Movies/index.js
@@ -1,10 +1,39 @@
-import { useState } from "react/cjs/react.development";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 import "./style.css";
 
-function FetchMovieCatalogFromServer(){
+function Movie({ movie }){
     return(
-        <div className="catalog"></div>
+        <section className="movie">
+            <Link to={`/sessoes/${movie.id}`}>
+                <img src={movie.posterURL} alt={movie.title} />
+            </Link>
+        </section>
+    );
+}
+
+function FetchMovieCatalogFromServer(){
+    const [movies, setMovies] = useState([]);
+    /*Usamos o 'useState' e iniciamos ele com uma array vazia. Depois,
+    usamos o 'useEffect' para visualizar qualquer mudança ocorrida
+    nessa array. Quando houver uma mudança array, o 'useEffect' é
+    atualizado e executa a função dentro dele.*/
+    useEffect(() => {
+        const promise = axios.get("https://mock-api.driven.com.br/api/v5/cineflex/movies");
+
+        promise.then((response) => {
+            setMovies(response.data);
+        });
+    }, [] );
+    
+    return(
+        <div className="catalog">
+            {movies.map((m) => {
+                return <Movie key={m.id} movie={m} />
+            })}
+        </div>
     );
 }
 
